5349bf383abc5c8e7ef5cdac4e7ffdbaf0c84125
AhmedSafwat97 <xppahmad@gmail.com>
2023-01-14 00:59:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# about the project
+this a scrolling landing page that contains a some of sections , this page let the user to scroll between sections smoothly and select the section from navbar to put the section in viewport screen , and the page contain a button to scroll the page to top .
+
+# Technologies used 
+Html 
+Css
+JavaScript
+
+
+# Table of Contents
+* Html and css from Udacity's startup project but i did some changes
+* using JavaScript :
+* built dynamically Navigation as an unordered list. Start with empty ul and dynamically build.
+* create Section Active State that clear which section is being viewed while scrolling through the page ,using "getBoundingClientRect()" 
+* add and remove active class when scrolling through the sections 
+* add a scroll to top button.
+
+# sources used 
+* https://developer.mozilla.org/en-US/docs/Glossary/Viewport
+* https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+* https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-behavior
+* https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+
+
+
+
+
+
+
+
---
+++ js/app.js
@@ -0,0 +1,79 @@
+/* Get the ul element */
+const navBar = document.getElementById("navbar__list")
+// Get all the sections on the page
+const Sections = Array.from(document.querySelectorAll("section"))
+
+function createSectionList () {
+    // Loop through the Sections array
+   Sections.forEach(sec => {
+    // Create a variable to store the HTML for the ul element
+        navElement = document.createElement("li");
+    // Use the innerHTML property to insert the HTML into the ul element
+        navElement.innerHTML = `<li> <a data-nav="#${sec.id}" class="menu__link"> ${sec.dataset.nav} </a> </li>`;
+         navBar.appendChild(navElement)   
+         scrollBehavior( navElement , sec);
+       });
+}
+
+createSectionList()
+
+/* Add an event listener to the window to listen for scroll events */
+window.addEventListener('scroll',(event)=>{
+    const activeLink = document.querySelectorAll(".menu__link")
+// Loop through all the sections
+    Sections.forEach((section, i)=>{
+// Get the bounding rect of the current section
+      const boxSec = section.getBoundingClientRect();
+// Check if the top of the section is within the viewport
+      if (boxSec.top <= 150 && boxSec.top >= -400)
+// Add the active-section class to the current section
+      { section.classList.add("your-active-class");
+          activeLink[i].classList.add("active-link");
+      } else{
+// Remove the active-section class 
+        section.classList.remove("your-active-class");
+        activeLink[i].classList.remove("active-link");
+      }
+  })
+
+
+// Declare a variable to check when the user is not scrolling
+    let isScrolling;
+// Assign setTimeout to the variable
+isScrolling = setTimeout(function(){ 
+// If the user hasn't scrolled while setTimeout is running then hide the fixed navigation bar
+    navBar.style.display = 'none';
+}, 200000000);
+
+// Cancels the timeout if the user scrolls again
+window.addEventListener('scroll', function(){
+    navBar.style.display = "block"
+    clearTimeout(isScrolling)
+});
+})
+
+//  smooth scroll when clicking on section button 
+function scrollBehavior( list, sec){
+    list.addEventListener('click', function(event){
+        event.preventDefault();
+        window.scrollTo({
+            top: sec.offsetTop,
+            behavior:"smooth"
+        });
+    });
+ }
+
+
+//  scroll to top of the page when the user click on the button
+document.getElementById("to-top").addEventListener('click', function(){
+    window.scrollTo({
+        top: 0,
+        behavior:"smooth"});
+});
+
+
+
+
+
+
+
